4ad15bfa249ac822a98d9801c4eead5a0e41b60e
jovana <stankovicmjovana@gmail.com>
2019-05-06 15:08:35 +0200

    Destroy action in sessions controller is done - logout done. Added link to welcome page, log in, log out and my profile in application layout nav bar. In application controller made authorize method and added skip_before_action in welcome controller and sessions controller.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,6 @@
 class SessionsController < ApplicationController
+	skip_before_action :authorize
+
   def new
   end
 
@@ -13,6 +15,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-
+  	session[:user_id] = nil
+  	redirect_to welcome_index_url, notice: "Logged out"
   end
 end
