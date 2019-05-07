77a39d699df999b1870c4ede20b63df4c981be6a
jovana <stankovicmjovana@gmail.com>
2019-05-07 15:09:18 +0200

    Created active_storage migration. In users view form partial added file_field to upload photo. Changed users show view to present the uploaded photo. In users controller in user_params permited :profile_image. In User model added has_one_attached :profile_image. It work only for profile with uploaded photo for now.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -80,7 +80,7 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:email, :password, :password_confirmation, :name)
+      params.require(:user).permit(:email, :password, :password_confirmation, :name, :profile_image)
     end
 
     def check_if_current_user
