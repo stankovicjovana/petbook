7317c51760e6f11c616974b056fb1e4f7bcd66e1
jovana <stankovicmjovana@gmail.com>
2019-05-08 15:37:28 +0200

    in model pet created method resize_images(image_index) and image_type. Added validate :image_type to be sure we upload jpg, png or jpeg files. In pet show view applied resize_images for all pets images. Image cannot be edited yet.

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -5,7 +5,22 @@ class Pet < ApplicationRecord
   validates :animal_type, presence: true
   validates :breed, presence: true
   has_many_attached :images
+  validate :image_type
+
   def owned_by(current_user) 
   	return user.id == current_user.id
   end
+
+  def resize_images(image_index)
+  	return self.images[image_index].variant(resize: '200X200!').processed
+  end
+
+  private 
+  	def image_type
+  		images.each do |image|
+  			if !image.content_type.in?(%('image/jpeg image/jpg image/png'))
+  				errors.add(:images, "Needs to be a JPG, JPEG or PNG")
+  			end
+  		end
+  	end
 end
