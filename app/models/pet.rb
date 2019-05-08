cf1053074d5c773cf9c0f6060ba81307b1ce6420
jovana <stankovicmjovana@gmail.com>
2019-05-08 11:33:07 +0200

    In user moded added method has_no_pets. Applied has_no_pets method in users show view to shop table with pets only if user has any pets. Added validations to pet model for name, description, animal_type and breed.

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,3 +1,7 @@
 class Pet < ApplicationRecord
   belongs_to :user
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :animal_type, presence: true
+  validates :breed, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -20,4 +20,8 @@ class User < ApplicationRecord
   		return false
   	end
   end
+
+  def has_no_pets
+    pets.count < 1
+  end
 end
