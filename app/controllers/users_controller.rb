02c945225869ffc7a43697f12ff0e18c44aac6a4
jovana <stankovicmjovana@gmail.com>
2019-05-08 13:00:17 +0200

    In users controller show action created @current_user variable. Used current_user variable in user show view to show edit and delete links in the pet table only if current user is looking to its own profile.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,7 @@ class UsersController < ApplicationController
   # GET /users/1.json
   def show
     @pets = @user.pets
+    @current_user = User.find(session[:user_id])
   end
 
   # GET /users/new
