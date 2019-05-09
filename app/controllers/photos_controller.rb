cc989c1b134f7b34aff7075a239ce314b1949327
jovana <stankovicmjovana@gmail.com>
2019-05-09 12:20:54 +0200

    In image _form added file_filed :pet_image to be able to attach photo, added :pet_image to photo_params in photos controller. In image show view added image_tag to show if there is attached photo. -No validations added yet, cat see this photos from pet show view yet.

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -73,6 +73,6 @@ class PhotosController < ApplicationController
     end
     # Never trust parameters from the scary internet, only allow the white list through.
     def photo_params
-      params.require(:photo).permit(:title, :description, :pet)
+      params.require(:photo).permit(:title, :description, :pet_image)
     end
 end
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,3 +1,4 @@
 class Photo < ApplicationRecord
 	belongs_to :pet
+	has_one_attached :pet_image
 end
