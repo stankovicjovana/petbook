85092294022528296239880b693b68c38de24f05
jovana <stankovicmjovana@gmail.com>
2019-05-10 16:13:40 +0200

    Rolled back last migration and added belongs_to :user for Post model.

--- db/migrate/20190510140918_create_posts.rb
+++ db/migrate/20190510140918_create_posts.rb
@@ -2,7 +2,8 @@ class CreatePosts < ActiveRecord::Migration[5.2]
   def change
     create_table :posts do |t|
       t.text :body
-
+      t.belongs_to :user, foreign_key: true
+      
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -62,8 +62,10 @@ ActiveRecord::Schema.define(version: 2019_05_10_140918) do
 
   create_table "posts", force: :cascade do |t|
     t.text "body"
+    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
